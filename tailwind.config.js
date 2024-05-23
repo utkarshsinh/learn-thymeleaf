daece0763ee23886128d4def3e6aefd15196d638
utkarshsinh <usinh002@ucr.edu>
2024-05-22 18:34:45 -0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ['./src/main/resources/templates/**/*.html'],
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: [],
+};
